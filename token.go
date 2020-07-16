a33ce555591f02d313a176f412447bb49f7f81bb
Darko Donnie <donni3thedarko@gmail.com>
2020-07-16 18:16:10 +0000

    token + lexer

--- token.go
+++ token.go
@@ -2,20 +2,23 @@ package token
 
 type TokenType string
 
-type Token struct {
-
-  Type TokenType
-  literal string
-
-}
-
 const (
   ILLEGAL = "ILLEGAL"
   EOF = "EOF"
   IDENT = "IDENT"
   INT = "INT"
+
   ASSIGN = "="
   PLUS = "+"
+  MINUS = "-"
+  ASTERISK = "*"
+  SLASH = "/"
+
+  LT  = "<"
+  GT =  ">"
+
+  EQ = "=="
+  NOT_EQ = "!="
   COMMA = ","
   SEMICOLON = ";"
   LPAREN = "("
@@ -24,6 +27,24 @@ const (
   RBRACE = "}"
   FUNCTION = "FUNCTION"
   LET = "LET"
+  TRUE = "TRUE"
+  FALSE = "FALSE"
+  IF = "IF"
+  ELSE = "ELSE"
+  RETURN = "RETURN"
+
+  type Token string {
+  
+    Type TokenType
+    Literal string
+  }
+
+  var keywords = map[string]TokenType {
+    if tok, ok := Keywords[ident]; ok {
+      return tok
+    }
+      return IDENT
+  }
 
 
 )
